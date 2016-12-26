c7687f00eb0c9862c4972c0a5323db0178a7056a
Sebastien Alborini <salborini@gmail.com>
2016-12-26 14:09:38 +0000

    fix

--- project/urls.py
+++ project/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
-from welcome.views import index, readyness, liveness
+from welcome.views import index, ready, live
 
 urlpatterns = [
     # Examples:
@@ -11,6 +11,5 @@ urlpatterns = [
     url(r'^$', index),
     url(r'^health/ready$', ready),
     url(r'^health/live$', live),
-    url(r'^health$', readyness),
     url(r'^admin/', include(admin.site.urls)),
 ]
